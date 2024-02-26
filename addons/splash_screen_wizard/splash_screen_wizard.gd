72834e06ebd1d99e758e8edc51868cc6b56e73a9
Pat <27511950+ThePat02@users.noreply.github.com>
2024-02-26 13:06:42 +0100

    add: Button for adding new slides

--- addons/splash_screen_wizard/splash_screen_wizard.gd
+++ addons/splash_screen_wizard/splash_screen_wizard.gd
@@ -2,9 +2,16 @@
 extends EditorPlugin
 
 
+var EditorUI = preload("res://addons/splash_screen_wizard/editor_ui.gd")
+
+
+var _inspector_plugin_editor_ui
+
+
 func _enter_tree():
-	pass
+	_inspector_plugin_editor_ui = EditorUI.new()
+	add_inspector_plugin(_inspector_plugin_editor_ui)
 
 
 func _exit_tree():
-	pass
+	remove_inspector_plugin(_inspector_plugin_editor_ui)
