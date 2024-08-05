fd4c84a1bd74cfcd77dd40ae6f923b5ae6f51e93
Pat <27511950+ThePat02@users.noreply.github.com>
2024-08-05 19:09:36 +0200

    misc

--- demo_splash_screen_wizard/slides/slide_godot.gd
+++
@@ -1,8 +0,0 @@
-extends SplashScreenSlide
-
-
-func _slide() -> void:
-	var animation_player = %AnimationPlayer
-	animation_player.play("wiggle")
-	await animation_player.animation_finished
-	slide_finished.emit()
